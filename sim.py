1a51fe96e761eebc2c5576db3d44450d2fd78865
Cassandra Dove <cdove@berkeley.edu>
2024-06-13 16:43:41 -0700

    Added full functionality to edit basic simulation and trigger variables from terminal, plus added informative overlay to plot

--- sim.py
+++ sim.py
@@ -1,6 +1,7 @@
 import numpy as np
 from collections import deque
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import sys
 from os import system
 
@@ -8,22 +9,26 @@ from os import system
 duration = 100                  # Duration of the simulation in seconds
 rate = 30                       # Rate of photon arrival per second
 size_list = 600                 # Length of the photon list queue
-significance_constant = 6       # Standard deviation multiplier for trigger algorithm
-tail = 4                        # Length of recent data to ignore when calculating running avg/variance for trigger
 running_avg_length = 3          # Length of running average
 
+# Default trigger parameters
+significance_constant = 6       # Standard deviation multiplier for trigger algorithm
+tail = 4                        # Length of recent data to ignore when calculating running avg/variance for trigger
+look_back_to = 17               # Length of time we look back through when calculating sigma for trigger threshold
 trigger_threshold_met = False   # Flag for trigger algorithm
-triggered_timestamp = -999 
+triggered_timestamp = -999      # Timestamp for when event-by-event is triggered
 
-np.random.seed(112)             # Seed for reproducibility
+show_peak_data = True
+random_seed = 112               # Seed for reproducibility
+np.random.seed(random_seed)     
 
-# Define the power law function.
+# Define the power law function - this is used to create random background energy
 def power_law(index, min_energy, max_energy):
     random_value = np.random.uniform(0, 1)
     energy = ((max_energy**(index + 1) - min_energy**(index + 1)) * random_value + min_energy**(index + 1))**(1/(index + 1))
     return energy
 
-# Define the Get_Energy function.
+# Define the Get_Energy function - this is used for random background energy
 def Get_Energy():
     index = -2.0                # Index of the spectrum.
     min_energy = 1.0            # Minimum energy.
@@ -84,26 +89,28 @@ class FixedLengthLIFOQueue:
         else:
             return 0                                   # Return 0 if the queue is empty
         
-# Gamma ray burst class with default values [Cassie]
+# Gamma ray burst class with default values
 class grb :
     peak_time = np.random.uniform(0.25 * duration, 0.75 * duration)
-    amplitude = 4
-    sigma = 3
+    amplitude = 5
+    sigma = 1
 
-    # Quick initialization of a GRB with default values that can be overridden if so desired [Cassie]
+    # Quick initialization of a GRB with default values that can be overridden if so desired
     def __init__(self, peak_time = np.random.uniform(0.25 * duration, 0.75 * duration), amplitude = 5, sigma = 1):
         self.peak_time = peak_time
         self.amplitude = amplitude
         self.sigma = sigma
 
-    # Returns a Gaussian curve with respect to time that can be added onto background noise to represent a GRB [Cassie]
+    # Returns a Gaussian curve with respect to time that can be added onto background noise to represent a GRB
     def burst_addition(self, curr_time):
         exponent = ((curr_time - self.peak_time) ** 2) / (-2 * self.sigma)
         return self.amplitude * np.exp(exponent)
 
+short_grb = grb(amplitude=20, sigma = 0.001)
+long_grb = [grb(peak_time=duration/2, amplitude=2, sigma=5), grb(peak_time=duration/2 + 5, amplitude=3, sigma=5), grb(peak_time=duration/2 + 10, amplitude=2, sigma=5)]
 
-default_grb = grb(50,4,0.05)
-bursts = [default_grb]
+bursts = []
+bursts += long_grb
 
 # Store data for plotting
 photon_count_data = []
@@ -117,6 +124,7 @@ def sim() :
     photon_count_data.clear()
     running_average.clear()
     photon_list_queue.clear()
+    np.random.seed(random_seed)
     current_time = 0
     accumulated_time = 0
     photon_count_in_last_second = 0
@@ -131,12 +139,21 @@ def sim() :
         current_time += time_to_next_event
         accumulated_time += time_to_next_event
 
+        for burst in bursts :
+            photon_count_in_last_second += burst.burst_addition(current_time)
+
+        if current_time > look_back_to :
+            look_back_std = np.std(running_average[(-1 * look_back_to) : (-1 * tail)])
+            threshold = running_average[-1 * tail] + significance_constant * look_back_std
+            
+            if (trigger_threshold_met == False and photon_count_data[-1] >= threshold) :
+                trigger_threshold_met = True
+                global triggered_timestamp
+                triggered_timestamp = current_time - tail
+
         if current_time < duration:
             photon_list_queue.appendleft((current_time, photon_energy))
             photon_count_in_last_second += 1
-
-            for burst in bursts :
-                photon_count_in_last_second += burst.burst_addition(current_time)
             
             if accumulated_time >= 1:
                 # Push the photon count for the last second into the photon count queues
@@ -147,16 +164,6 @@ def sim() :
 
                 # Store data for plotting
                 photon_count_data.append(photon_count_in_last_second)
-
-                look_back_to = 17
-                if current_time > look_back_to :
-                    # 3s test
-                    look_back_std = np.std(running_average[-1 * look_back_to : -1 * tail])
-
-                    if (trigger_threshold_met == False and photon_count_data[-1] >= (running_average[-1 * tail] + (significance_constant * look_back_std))) :
-                        trigger_threshold_met = True
-                        global triggered_timestamp
-                        triggered_timestamp = current_time - tail
 
                 # Reset for the next second
                 photon_count_in_last_second = 0
@@ -172,8 +179,14 @@ def display_plots() :
     # Calculate errors for photon count data
     photon_count_errors = [np.sqrt(count) for count in photon_count_data]
 
-    # Plotting the results
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12))
+    fig = plt.figure(figsize=(10,6))
+    gs = gridspec.GridSpec(2, 2, width_ratios=[3, 1])
+
+    ax1 = fig.add_subplot(gs[0,0])
+    ax2 = fig.add_subplot(gs[1,0])
+
+    # # Plotting the results
+    # fig, axs = plt.subplots(2, 1, figsize=(12, 10))
 
     # Plot photon counts with error bars
     ax1.errorbar(range(len(photon_count_data)), photon_count_data, yerr=photon_count_errors, fmt='o', label='Photon Count')
@@ -181,29 +194,38 @@ def display_plots() :
     ax1.set_ylabel('Photon Count')
     ax1.set_title('Photon Count per Second')
     ax1.set_ylim(y_range_min, y_range_max)  # Set the same y-axis range for all plots
+    
+    # Create labeled dashed lines for each peak
+    if show_peak_data :
+        i = 1
+        for burst in bursts : 
+            ax1.vlines(burst.peak_time, y_range_min, y_range_max, label='Burst ' + str(i), color='green', linestyles='dashed')
+            i += 1
     ax1.legend()
 
     # Plot running average
     ax2.plot(running_average, 'o', label='Running Average (' + str(running_avg_length) + ' seconds)', color='orange')
     ax2.vlines(triggered_timestamp, y_range_min, y_range_max, label='Threshold Triggered')
+    ax2.annotate(str(round(triggered_timestamp,2)) + 's', (triggered_timestamp + 0.5, y_range_max / 2), xycoords='data', xytext=(-0.5,0), textcoords='offset fontsize', ha='right')
     ax2.set_xlabel('Time (seconds)')
     ax2.set_ylabel('Running Average')
     ax2.set_title('Running Average of Photon Count (' + str(running_avg_length) + ' seconds)')
     ax2.set_ylim(y_range_min, y_range_max)  # Set the same y-axis range for all plots
     ax2.legend()
 
-    for burst in bursts :
-        print("GRB at " + str(burst.peak_time) + " seconds with amplitude " + str(burst.amplitude) + " and sigma value " + str(burst.sigma))
 
-    print("Event-by-event triggered at " + str(triggered_timestamp))
-    plt.tight_layout(pad=3)
+    # Add textbox with variable information
+    #textbox_ax = fig.add_axes([0.85, 0.2, 0.1, 0.6])
+    textbox_ax = fig.add_subplot(gs[:, 1])
+    textbox_ax.axis('off')
+    textbox_ax.text(0.1, 0.5, all_variables_str(), verticalalignment='center', horizontalalignment='left')
+    #plt.constrained_
+    plt.tight_layout()
     plt.show()
 
 def change_running_average_length() :
     global running_avg_length
-    mod = str(
-        input('\nCurrent running average length is ' + str(running_avg_length) + 's. Would you like to modify it? (y/n) ')
-    )
+    mod = ''
     while (mod != 'y' and mod != 'n') :
         mod = str(
             input('\nCurrent running average length is ' + str(running_avg_length) + 's. Would you like to modify it? (y/n) ')
@@ -211,16 +233,14 @@ def change_running_average_length() :
     if mod == 'n' :
         modify_variables()
     else :
-        new_running_avg = int(
+        running_avg_length = int(
             input('\nEnter the new running average length: ')
         )
-        running_avg_length = new_running_avg
+        print('\nRunning average length has been changed to ' + str(running_avg_length) + 's!')
 
 def change_rate() :
     global rate
-    mod = str(
-        input('\nCurrent rate is ' + str(rate) + ' photons/s. Would you like to modify it? (y/n) ')
-    )
+    mod = ''
     while (mod != 'y' and mod != 'n') :
         mod = str(
             input('\nCurrent rate is ' + str(rate) + ' photons/s. Would you like to modify it? (y/n) ')
@@ -228,16 +248,14 @@ def change_rate() :
     if mod == 'n' :
         modify_variables()
     else :
-        new_rate = int(
+        rate = int(
             input('\nEnter the new rate: ')
         )
-        rate = new_rate
+        print('\nRate has been changed to ' + str(rate) + ' photons/s!')
 
 def change_duration() :
     global duration
-    mod = str(
-        input('\nCurrent duration is ' + str(duration) + 's. Would you like to modify it? (y/n) ')
-    )
+    mod = ''
     while (mod != 'y' and mod != 'n') :
         mod = str(
             input('\nCurrent duration is ' + str(duration) + 's. Would you like to modify it? (y/n) ')
@@ -248,12 +266,11 @@ def change_duration() :
         duration = int(
             input('\nEnter the new duration: ')
         )
+        print('\nDuration has been changed to ' + str(duration) + 's!')
 
 def change_photon_list_length() :
     global size_list
-    mod = str(
-        input('\nCurrent size of photon list is ' + str(size_list) + ' photons. Would you like to modify it? (y/n) ')
-    )
+    mod = ''
     while (mod != 'y' and mod != 'n') :
         mod = str(
             input('\nCurrent size of photon list is ' + str(size_list) + ' photons. Would you like to modify it? (y/n) ')
@@ -264,12 +281,11 @@ def change_photon_list_length() :
         size_list = int(
             input('\nEnter the new photon list length: ')
         )
+        print('\nPhoton list length has been changed to ' + str(size_list) + ' photons!')
 
 def change_significance_constant() :
     global significance_constant
-    mod = str(
-        input('\nCurrent significance constant is ' + str(significance_constant) + '. Would you like to modify it? (y/n) ')
-    )
+    mod = ''
     while (mod != 'y' and mod != 'n') :
         mod = str(
             input('\nCurrent significance constant is ' + str(significance_constant) + '. Would you like to modify it? (y/n) ')
@@ -280,12 +296,11 @@ def change_significance_constant() :
         significance_constant = int(
             input('\nEnter the new significance constant: ')
         )
+        print('\nSignificance constant has been changed to ' + str(significance_constant) + '!')
 
 def change_tail_length() :
     global tail
-    mod = str(
-        input('\nCurrent tail is ' + str(tail) + 's. Would you like to modify it? (y/n) ')
-    )
+    mod = ''
     while (mod != 'y' and mod != 'n') :
         mod = str(
             input('\nCurrent tail is ' + str(tail) + 's. Would you like to modify it? (y/n) ')
@@ -296,16 +311,113 @@ def change_tail_length() :
         tail = int(
             input('\nEnter the new tail: ')
         )
+        print('\nTail has been changed to ' + str(tail) + 's!')
 
-def display_all_variables() : 
-    print('\nCurrent settings:')
-    print('\t1. Simulation duration (in seconds) : ' + str(duration))
-    print('\t2. Rate of photon arrival per second: ' + str(rate))
-    print('\t3. Length of the photon list queue: ' + str(size_list))
-    print('\t4. Significance constant: ' + str(significance_constant))
-    print('\t5. Tail length (in seconds): ' + str(tail))
-    print('\t6. Running average length (in seconds): ' + str(running_avg_length))
-    print('\t7. Number of peaks: ' + str(len(bursts)))
+def change_look_back_to() :
+    global look_back_to
+    mod = ''
+    while mod != 'y' and mod != 'n' :
+        mod = str(
+            input('\nCurrent look_back_to is ' + str(look_back_to) + 's. Would you like to modify it? (y/n) ') 
+        )
+    if mod == 'n' :
+        modify_variables()
+    else :
+        look_back_to = int(
+            input('\nEnter the new look_back_to: ')
+        )
+        print('\nLook_back_to has been changed to ' + str(look_back_to) + 's!')
+
+# tab function (pyplot won't allow \t)
+def t() :
+    return '     '
+
+def press_any_key_to_continue() :
+    i = ''
+    while i == '' :
+        i = input('Press any key, then ENTER, to continue: ')
+
+def basic_sim_variables_str() : 
+    summary = ( '\nBasic simulation variables: ' + 
+                '\n' + t() + 'Simulation duration (in seconds) : ' + str(duration) + 
+                '\n' + t() + 'Rate of photon arrival per second: ' + str(rate) + 
+                '\n' + t() + 'Length of the photon list queue: ' + str(size_list) +
+                '\n' + t() + 'Running average length (in seconds): ' + str(running_avg_length))
+    return summary
+
+def trigger_variables_str() : 
+    summary = ( '\nTrigger Variables: ' + 
+                '\n' + t() + 'Significance constant: ' + str(significance_constant) + 
+                '\n' + t() + 'Tail length (in seconds): ' + str(tail) + 
+                '\n' + t() + 'Lookback duration (in seconds): ' + str(look_back_to))
+    return summary
+
+def burst_info_str() : 
+    summary = '\n' + 'Burst information: '
+    i = 0
+    for burst in bursts :
+        summary += ('\n' + t() + 'Peak ' + str(i) + '\n' + t() + t() + 'Peaks at ' + str(round(burst.peak_time, 2)) + 's\n' + t() + t() + 'A = ' + str(burst.amplitude) + '\n' + t() + t() + 'Sigma = ' + str(burst.sigma))
+        i += 1
+    return summary
+
+def display_burst_info() :
+    print(burst_info_str())
+
+def delete_burst_helper(i) :
+    del bursts[i]
+
+def delete_burst() :
+    system('cls')
+    display_burst_info()
+    ans = ''
+    while ans != 'y' and ans != 'n' :
+        ans = input(
+            '\nWould you like to delete a burst? (y/n) '
+        )
+    if ans == 'y' :
+        delete_index = int(
+            input('\nEnter the number of the burst you\'d like to delete: ')
+        )
+        delete_burst_helper(delete_index)
+        print('\nBurst ' + str(delete_index) + ' was sucessfully deleted.')
+        press_any_key_to_continue()
+    
+
+def all_variables_str() : 
+    summary = basic_sim_variables_str() + '\n' + trigger_variables_str()
+    if show_peak_data :
+        summary += '\n' + burst_info_str()
+    return summary
+
+def display_all_variables() :
+    system('cls')
+    print('\nCurrent settings: ')
+    print(all_variables_str())
+    x = 999
+    while x != '0' and x != 1 :
+        x = str(
+            input('\nEnter 0 to modify variables, or 1 to return to main menu: ')
+        )
+    if x == '0' :
+        modify_variables()
+    else :
+        main()
+
+def modify_bursts() :
+    functions_names = [display_burst_info, delete_burst, return_to_modification_menu, return_to_menu]
+    menu_items = dict(enumerate(functions_names, start=0))
+    while True:
+        system('cls')
+        print('\nBurst Modification Menu: ')
+        display_menu(menu_items)
+        selection = int(
+            input("Please enter your selection number: ")
+        )
+        selected_value = menu_items[selection]
+        selected_value()
+
+def return_to_modification_menu() :
+    modify_variables()
 
 def exit() : 
     system('cls')  # clears stdout
@@ -315,18 +427,33 @@ def exit() :
 def return_to_menu() :
     main()
 
-def modify_variables() : 
-    functions_names = [display_all_variables, change_duration, change_rate, change_photon_list_length, change_significance_constant, 
-                       change_tail_length, change_running_average_length, return_to_menu]
+def modify_variables() :
+    system('cls')
+    functions_names = [display_all_variables, change_duration, change_rate, change_photon_list_length, change_running_average_length, 
+                       change_significance_constant, change_tail_length, change_look_back_to, modify_bursts, return_to_menu]
     menu_items = dict(enumerate(functions_names, start=0))
     while True:
-        print('\nMENU')
         print('\nModification options: ')
         display_menu(menu_items)
         selection = int(
             input("Please enter your selection number: "))      # Get function key
         selected_value = menu_items[selection]                  # Gets the function name
         selected_value()                                        # add parentheses to call the function
+
+def run_simulation_and_plot() :
+    global show_peak_data
+    selection = ''
+    while selection != 'y' and selection != 'n' :
+        selection = str(
+            input('Show peak data? (y/n) ')
+        )
+    if selection == 'y' :
+        show_peak_data = True
+    else :
+        show_peak_data = False
+    system('cls')
+    sim()
+    display_plots()
 
 def display_menu(menu):
     """
@@ -340,14 +467,15 @@ def display_menu(menu):
 def main():
     # Create a menu dictionary where the key is an integer number and the
     # value is a function name.
-    functions_names = [sim, display_plots, display_all_variables, modify_variables, exit]
+    functions_names = [run_simulation_and_plot, display_all_variables, modify_variables, exit]
     menu_items = dict(enumerate(functions_names, start=1))
+    system('cls')
 
     while True:
-        print('\nMENU')
+        print('MENU')
         display_menu(menu_items)
         selection = int(
-            input("Please enter your selection number: "))      # Get function key
+            input("\nPlease enter your selection number: "))      # Get function key
         selected_value = menu_items[selection]                  # Gets the function name
         selected_value()                                        # add parentheses to call the function
 
