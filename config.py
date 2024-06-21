ca4d603491737d53f8a3b32809cd719e980799bc
Cassandra Dove <cdove@berkeley.edu>
2024-06-21 11:26:06 -0700

    Refined sgrb, lgrb parameters

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class GlobalState:
             'running_avg_length': 3,
             'ebe_bin_length': 0.04,
             'enter_significance_constant': 6,
-            'tail': 4,
+            'tail': 7,
             'enter_look_back_to': 17,
             'trigger_threshold_met': False,
             'triggered_timestamp': -999,
@@ -18,10 +18,10 @@ class GlobalState:
             'exit_look_back_to': 15,
             'show_peak_data': True,
             'random_seed': 112,
-            'sgrb_A': 20,
-            'sgrb_sigma': 0.001,
-            'lgrb_A': 2,
-            'lgrb_sigma': 5
+            'sgrb_A': 10,
+            'sgrb_sigma': 0.25,
+            'lgrb_A': 3,
+            'lgrb_sigma': 10
         }
 
     def save(self, filename):
